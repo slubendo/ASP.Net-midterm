405be0d50ed6abb920eb17d2e4ef06f2e2a38d13
slubendo <stephane.lubendo@gmail.com>
2024-01-29 13:19:20 -0800

    form done, everything up until lunch

--- frontend/src/routes/new-expense.tsx
+++ frontend/src/routes/new-expense.tsx
@@ -1,10 +1,12 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { useNavigate } from "@tanstack/react-router";
-import { createFileRoute } from "@tanstack/react-router";
+import { useNavigate, createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
+import { useForm } from "@tanstack/react-form";
+import { zodValidator } from "@tanstack/zod-form-adapter";
+import { z } from "zod";
 
 
 
@@ -12,21 +14,32 @@ export const Route = createFileRoute("/new-expense")({
   component: NewExpensePage,
 });
 
-type Expense = {
-  id: number;
-  title: string;
-  total: number;
-};
+const ExpsenseType = z.object({
+  title: z.string().min(3).max(100),
+  total: z.number().min(0).max(1000),
+});
+type ExpsenseType = z.infer<typeof ExpsenseType>;
 
 function NewExpensePage() {
-  const [title, setTitle] = useState("");
-  const [total, setTotal] = useState(0);
+  const form = useForm({
+    defaultValues: {
+      title: "",
+      total: 0,
+    },
+    onSubmit: async ({ value }) => {
+      // wait
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+      await mutateAsync(value);
+      navigate({ to: "/all-expenses" });
+    },
+    validatorAdapter: zodValidator,
+  });
 
   const navigate = useNavigate({ from: "/new-expense" });
 
   const { error, isPending, mutateAsync } = useMutation({
     mutationKey: ["postTotal"],
-    mutationFn: async ({ title, total }: Expense) => {
+    mutationFn: async ({ title, total }: ExpsenseType) => {
       const response = await fetch("/api/Expenses", {
         method: "POST",
         headers: {
@@ -38,48 +51,83 @@ function NewExpensePage() {
     },
   });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      await mutateAsync({
-        id: 0,
-        title: title,
-        total: total,
-      });
-      // Reset form fields after successful submission
-      setTitle("");
-      setTotal(0);
-      navigate({ to: "/all-expenses" });
-    } catch (error) {
-      console.error("Error adding expense:", error);
-    }
-  };
 
-  return (
-    <>
-      {isPending ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error: try again later</p>
-      ) : (
-        <>
-          <h1 className="text-2xl">New Expense</h1>
+    return (
+      <>
+        {isPending ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: try again later</p>
+        ) : (
+          <>
+            <h1 className="text-2xl">New Expense</h1>
+            <form.Provider>
+              <form className="flex flex-col gap-y-10" onSubmit={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                void form.handleSubmit();
+              }}>
+                <form.Field
+                  name="title"
+                  validators={{
+                    onChange: ExpsenseType.shape.title,
+                  }}
+                  children={(field) => (
+                    <>
+                      <Label className="block mb-2">Title</Label>
+                      <Input
+                        className="border border-gray-400 rounded px-2 py-1 mb-4"
+                        type="text"
+                        placeholder="Type"
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                      />
+                      {field.state.meta.errors && (
+                        <em role="alert">{field.state.meta.errors.join(", ")}</em>
+                      )}
+                    </>
+                  )}
+                />
 
-          <form className="flex flex-col gap-y-10" onSubmit={handleSubmit}>
-            <Label>
-              Title
-              <Input value={title} onChange={(e) => setTitle(e.target.value)} />
-            </Label>
+                <form.Field
+                  name="total"
+                  validators={{
+                    onChange: ExpsenseType.shape.total,
+                  }}
+                  children={(field) => (
+                    <>
+                      <Label className="block mb-2">total</Label>
+                      <Input
+                        className="border border-gray-400 rounded px-2 py-1 mb-4"
+                        type="text"
+                        placeholder="Type"
+                        value={field.state.value}
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(parseInt(e.target.value))}
+                      />
+                      {field.state.meta.errors && (
+                        <em role="alert">{field.state.meta.errors.join(", ")}</em>
+                      )}
+                    </>
+                  )}
+                />
 
-            <Label>
-              Amount
-              <Input type="number" value={total} onChange={(e) => setTotal(parseInt(e.target.value))} />
-            </Label>
 
-            <Button type="submit">
-              Submit
-            </Button>
-          </form>
+              <form.Subscribe
+                selector={(state) => [state.canSubmit, state.isSubmitting]}
+                children={([canSubmit, isSubmitting]: [boolean, boolean]) => (
+                  <Button
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                    type="submit"
+                    disabled={!canSubmit || isSubmitting}
+                  >
+                    {isSubmitting ? "..." : "Submit"}
+                  </Button>
+                )}
+              ></form.Subscribe>
+            </form>
+          </form.Provider>
         </>
       )}
     </>
